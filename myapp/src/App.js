48ce85dadbaed85f4e18fb9bfa171d3d55defca5
urmila2801 <urmilapamarthi3@gmail.com>
2023-06-03 06:04:59 +0000

    functional Component

---
+++ myapp/src/App.js
@@ -0,0 +1,26 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>
+          <h1 className="app-heading">Started A New Topic.....</h1>
+          <p className="app-p">Hello React...</p>
+        </div>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
